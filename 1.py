8beab1bb3f398e26a519fb8603316c4fb6725de9
gbrs <gbrs@mail.ru>
2024-05-28 01:02:09 +0300

    solve task 1

--- 1.py
+++ 1.py
@@ -1,10 +1,22 @@
 class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
-        return 42, 42
+        s = set(nums)
+        digits = list(s.intersection([target - i for i in s]))
+        # print(*digits)
+        if len(digits) == 1:
+            answer_1 = nums.index(digits[0])
+            answer_2 = nums.index(digits[0], answer_1 + 1)
+        elif len(digits) == 2:
+            answer_1 = nums.index(digits[0])
+            answer_2 = nums.index(digits[1])
+        else:
+            answer_1 = nums.index(min(digits))
+            answer_2 = nums.index(max(digits))
+        return [answer_1, answer_2]
 
 
-lst = [100500, 42, 13, 7, 42, 2]
-t = 84
-answer = Solution()
-print(*answer.twoSum(lst, t))
+lst = [3, 2, 4]
+t = 6
+a = Solution()
+print(*a.twoSum(lst, t))
 
