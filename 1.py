9efdcb1e48aed655e9768561496b6101923eb192
gbrs <gbrs@mail.ru>
2024-05-27 20:07:59 +0300

    start task 1

---
+++ 1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        return 42, 42
+
+
+lst = [100500, 42, 13, 7, 42, 2]
+t = 84
+answer = Solution()
+print(*answer.twoSum(lst, t))
+
